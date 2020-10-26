5594717155c11eaaf3bb50d8b2f64f4f71422d24
Fredy <fredy.ly.wcs@gmail.com>
2020-10-26 22:15:55 +0100

    adding challenge_react_07 from Odyssey

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import QuoteCard from './components/QuoteCard';
+import axios from 'axios';
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const quotes = 
+  {
+    quote:"I can't even say the word 'titmouse' without gigggling like a schoolgirl.",
+    character:"Homer Simpson",
+    image:"https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+    characterDirection:"Right"
+  }
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      simpsons: quotes
+    }
+    this.getQuotes = this.getQuotes.bind(this);
+  }
+
+  getQuotes() {
+    // Send the request
+    axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then(response => response.data)
+      // Use this data to update the state
+      .then(data => {
+        console.log('data :', data)
+        this.setState({
+          simpsons: data[0],
+        });
+    });
+  }
+
+    render() {
+      return (
+        <div>
+          <QuoteCard simpsons={this.state.simpsons} />
+          <button type="button" onClick={this.getQuotes}>Get a new quote</button>
+          </div>
+      );
+    }
 }
+
+
+
 
 export default App;
